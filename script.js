0bafeb35d1dfad8f89e43a3721c18661b96d151b
Syed Arslan <arslanhaider231@gmail.com>
2024-10-02 19:01:42 +0200

    i add burger button inside both pages when mobile view

---
+++ script.js
@@ -0,0 +1,16 @@
+const burgerButton = document.getElementById('burger-button');
+const nav = document.querySelector('.nav-header');
+const body = document.querySelector('body');
+const burgerIcon = document.querySelector('.burger-icon');
+
+burgerButton.addEventListener('click', () => {
+    nav.classList.toggle('active');
+    
+    if (nav.classList.contains('active')) {
+        body.style.overflow = 'hidden';  
+        burgerIcon.innerHTML = '&#10006;'; // Close icon
+    } else {
+        body.style.overflow = 'auto'; 
+        burgerIcon.innerHTML = '&#9776;'; // Burger icon
+    }
+});
